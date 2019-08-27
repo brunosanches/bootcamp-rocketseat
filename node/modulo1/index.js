0eaf853071f45d78128bd246579b44d66ff4e186
Bruno Sanches <bruno@moneuts.com.br>
2019-08-26 21:53:24 -0300

    Adicionando nodemon

--- node/modulo1/index.js
+++ node/modulo1/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/users', (req, res) => {
-  const { nome } = req.query;
-  return res.json({ message: `Nome do usuário é: ${nome}` })
-});
+const users = ['Bruno Filho', 'Íris', 'Luciana', 'Bruno'];
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  return res.json({ message: `Buscando id: ${id}` })
-})
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
 
 server.listen(3000);
