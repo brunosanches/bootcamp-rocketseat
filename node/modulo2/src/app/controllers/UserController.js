bf1f5eb20a0b62bf33a171e85cafe34e68148a70
Bruno Sanches <bruno@moneuts.com.br>
2019-09-02 23:34:37 -0300

    Adicionando autenticacao com jwt

---
+++ node/modulo2/src/app/controllers/UserController.js
@@ -0,0 +1,48 @@
+import User from '../models/User'
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } })
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' })
+    }
+
+    const { id, name, email, provider } = await User.create(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    })
+  }
+
+  async update(req, res) {
+    const { email, oldPasswaord } = req.body
+
+    const user = await User.findByPk(req.userId)
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } })
+      if (userExists) {
+        res.status(401).json({ error: 'User already exists' })
+      }
+    }
+
+    if (oldPasswaord && !(await user.checkPassword(oldPasswaord))) {
+      res.status(401).json({ error: 'Password does nor match' })
+    }
+
+    const { id, email, name, provider } = await user.update(req.body)
+
+    return res.json({
+      id,
+      email,
+      name,
+      provider
+    })
+  }
+}
+
+export default new UserController()
---
+++ node/modulo2/src/app/middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+
+import authConfig from '../../config/auth'
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    res.status(401).json({ error: 'Token not provided' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  console.log(authHeader)
+
+  try {
+    const { id } = await promisify(jwt.verify)(token, authConfig.secret)
+
+    req.userId = id
+
+    return next()
+  } catch (error) {
+    res.status(401).json({ error: 'Token invalid' })
+  }
+
+  console.log(authHeader)
+
+  return next()
+}
